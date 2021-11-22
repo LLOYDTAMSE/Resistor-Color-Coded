11c808f4604ac68bbdf9dd4d1ac0ca120aeaa38c
gisketch <agjmnz@gmail.com>
2021-11-22 23:02:06 +0800

    disabling and enabling 3rd band thru states

--- Calculator/Assets/Scripts/Calculator.cs
+++ Calculator/Assets/Scripts/Calculator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Calculator : MonoBehaviour
@@ -15,6 +16,11 @@ public class Calculator : MonoBehaviour
     public ResistorState resistorState;
     public TMP_Dropdown dropdown;
 
+    [Header("Band Groups")]
+    public Transform firstBandGroup;
+    public Transform secondBandGroup;
+    public Transform thirdBandGroup;
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +30,21 @@ public class Calculator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(resistorState == ResistorState.Four)
+        {
+            //If four band, disable all 3rd bands
+            foreach(Transform child in thirdBandGroup)
+            {
+                child.GetComponent<Button>().interactable = false;
+            }
+        } else
+        {
+            //If five band, enable all 3rd bands
+            foreach(Transform child in thirdBandGroup)
+            {
+                child.GetComponent<Button>().interactable = true;
+            }
+        }
     }
 
     public void OnStateChange()
