69bc1e30ef652e9e2ad1e277aa9568ae1f40af54
gisketch <agjmnz@gmail.com>
2021-11-22 22:47:30 +0800

    resistor states using enum and dropdown

--- Calculator/Assets/Scripts/Calculator.cs
+++ Calculator/Assets/Scripts/Calculator.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Calculator : MonoBehaviour
 {
+
+    public enum ResistorState {
+        Four,
+        Five
+    }
+
+    [Header("Resistor State")]
+    public ResistorState resistorState;
+    public TMP_Dropdown dropdown;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("HEllo");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnStateChange()
+    {
+        if(dropdown.value == 0)
+        {
+            resistorState = ResistorState.Four;
+        } else
+        if(dropdown.value == 1)
+        {
+            resistorState = ResistorState.Five;
+        }
     }
 }
