9679762287ddc4ecfb6c23b633bbe7863837b067
LLOYDTAMSE <94174266+LLOYDTAMSE@users.noreply.github.com>
2021-11-22 22:34:23 +0800

    Update Calculator.cs

--- Calculator/Assets/Scripts/Calculator.cs
+++ Calculator/Assets/Scripts/Calculator.cs
@@ -7,7 +7,7 @@ public class Calculator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("HEllo");
     }
 
     // Update is called once per frame
